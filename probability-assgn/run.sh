145a8544f36399027f225331c7b0c0a51772b17d
Sairaghavendra36 <68774753+Sairaghavendra36@users.noreply.github.com>
2023-01-04 11:00:01 +0530

    Delete probability-assgn directory

--- probability-assgn/run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#cd tables/
-##create the table as .xlsx
-##then convert it to .tex file
-#ssconvert --export-type=Gnumeric_html:latex table1.xlsx table1.tex
-#cd.. 
-texfot pdflatex table1.tex
-termux-open table1.pdf
